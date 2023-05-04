9d08c19570fe7b240a0b32299175325d4356ca2d
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-05-04 09:56:29 +0200

    init commit

---
+++ DemoLibrary/DisplayMessages.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class DisplayMessages
+    {
+        public string Greeting(string firstName, int hourOfTheDay)
+        {
+            string output = string.Empty;
+            if (hourOfTheDay < 12)
+            {
+                output = $"Good Morning { firstName } ";
+            }
+        }
+    }
+}
