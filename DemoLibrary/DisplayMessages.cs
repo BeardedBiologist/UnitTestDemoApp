907cae0abae6a63fab842119a810da1881b98ac1
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-05-04 10:50:34 +0200

    Unit Testing using Xunit;  // Arrange, Act, Assert

--- DemoLibrary/DisplayMessages.cs
+++ DemoLibrary/DisplayMessages.cs
@@ -11,10 +11,24 @@ namespace DemoLibrary
         public string Greeting(string firstName, int hourOfTheDay)
         {
             string output = string.Empty;
-            if (hourOfTheDay < 12)
+            if (hourOfTheDay < 5)
             {
-                output = $"Good Morning { firstName } ";
+                output = $"Go to bed {firstName}";
             }
+            else if (hourOfTheDay < 12)
+            {
+                output = $"Good morning { firstName }";
+            } 
+            else if (hourOfTheDay < 18)
+            {
+                output = $"Good afternoon {firstName}";
+            }
+            else
+            {
+                output = $"Good evening {firstName}";
+            }
+
+            return output;
         }
     }
 }
